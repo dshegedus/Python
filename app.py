400fb64a1439b39ad6f47ec6f69292466efdd482
dshegedus <dshegedus@yahoo.com>
2020-03-06 17:40:57 +0000

    V0.01

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 ### Transport 11, Meal 5
-
 transport_fee = 11
 meal_fee = 5
 menu_decision = 0
 
 while int(menu_decision) != 1 or int(menu_decision) != 2 or int(menu_decision) != 3:
+    menu_decision = input("Select an option from the menu by typing in its number. \n 1. See Current Savings. \n 2. Add Savings. \n 3. Add a Wish. \n...   ")
     try:
-        menu_decision = int(input("Select an option from the menu by typing in its number. \n 1. See Current Savings. \n 2. Add Savings. \n 3. Add a Wish. \n...   "))
+        int(menu_decision)
     except ValueError:
-        print("Oops! Please give a valid number from the menu.")
+        print("Oops! Please give a valid number from the menu. \n")
+        menu_decision = 0
         continue
-    break
-        
+    if int(menu_decision) == 1 or int(menu_decision) == 2 or int(menu_decision) == 3: 
+        break
+
 if int(menu_decision) == 1:
-    print("this")
+    print("1. Current Savings.")
+    read_only = open("data.py", "r")
+    print (read_only.read())
 elif int(menu_decision) == 2:
-        print("this2")
+    print("this2")
 elif int(menu_decision) == 3:
-        print("this3")
+    print("this3")
---
+++ var.py
@@ -0,0 +1,6 @@
+### Variables and the Dictionary of items and their values.
+transport_fee = 11
+meal_fee = 5
+menu_decision = 0
+wishes = {"bicycle" : 300, "Wedding" : 300, "Easter" : 300, "Damon" : 80, "Laptop":1000}
+
