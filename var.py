9c2650050c55c657d1920767157312af5065b095
dshegedus <dshegedus@yahoo.com>
2020-03-06 23:23:39 +0000

    changing files to CSV

--- var.py
+++
@@ -1,6 +0,0 @@
-### Variables and the Dictionary of items and their values.
-transport_fee = 11
-meal_fee = 5
-menu_decision = 0
-wishes = {"bicycle" : 300, "Wedding" : 300, "Easter" : 300, "Damon" : 80, "Laptop":1000}
-
